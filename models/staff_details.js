f393ad54e24b66d267a1eea6119999f8b1f9ffc1
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-10-11 03:44:10 +0800

    staff_details microservice, show all details and based off staff_id number. Finding a way to display retreive data based on other parameters without hardcoding.

--- models/staff_details.js
+++ models/staff_details.js
@@ -55,3 +55,89 @@ sequelize.sync({ force: false }) // Set force to true to drop existing tables an
   });
 
 module.exports = StaffDetails;
+
+// --Microservice Portion--
+const express = require('express');
+const mysql2 = require('mysql2');
+
+const app = express();
+const port = 5004;
+
+
+app.get('/staff_details', async (req, res) => {
+  try {
+    const staff_details = await StaffDetails.findAll();
+
+    if (staff_details.length) {
+      return res.status(200).json({
+        code: 200,
+        data: {
+          'staff_details': staff_details.map(staff_details => staff_details.toJSON()),
+        },
+      });
+    }
+    return res.status(404).json({
+      code: 404,
+      message: 'There are no Staff details.',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+app.get('/staff_details/:staff_id', async (req, res) => {
+  const staff_id = req.params.staff_id;
+  // const { fname, lname, dept, email } = req.query;
+  // const whereClause = {};
+
+  // Build the WHERE clause based on provided parameters
+  // if (staff_id) {
+  //   whereClause.staff_id = staff_id;
+  // }
+  // if (fname) {
+  //   whereClause.fname = fname;
+  // }
+  // if (lname) {
+  //   whereClause.lname = { [Op.like]: `%${lname}%` };
+  // }
+  // if (dept) {
+  //   whereClause.dept = dept;
+  // }
+  // if (email) {
+  //   whereClause.email = email;
+  // }
+
+  try {
+    const staff_details = await StaffDetails.findAll({ where: {staff_id} });
+
+    if (staff_details && staff_details.length > 0) {
+      return res.status(200).json({
+        code: 200,
+        data: {
+          'staff_details': staff_details.map(staff => staff.toJSON()),
+        },
+      });
+    }
+
+    return res.status(404).json({
+      code: 404,
+      message: 'No matching staff details found.',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
