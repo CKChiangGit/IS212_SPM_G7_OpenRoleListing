30f994f4721082e719d967bd8811b4662ba3440f
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-10-11 02:01:33 +0800

    Added microservice portion for staff_roles.js and staff_skills.js not sure if it works yet

--- models/staff_roles.js
+++ models/staff_roles.js
@@ -31,3 +31,46 @@ StaffRoles.belongsTo(StaffDetails, { foreignKey: 'staff_id', as: 'staff' });
 StaffRoles.belongsTo(RoleDetails, { foreignKey: 'staff_role', as: 'role' });
 
 module.exports = StaffRoles;
+
+// --Microservice Portion--
+const express = require('express');
+const cors = require('cors');
+const Sequelize = require('sequelize');
+
+const app = express();
+const port = 5003;
+
+app.use(cors());
+
+const sequelize = new Sequelize(process.env.dbURL || 'mysql://root@localhost:3306/spm', {
+  logging: false,
+});
+
+app.get('/staff_roles/:staff_id', async (req, res) => {
+  try {
+    const staff_role = await staff_roles.findOne({where: {staff_id}});
+    if (staff_role) {
+      return res.status(200).json({
+        code: 200,
+        data: {
+          'staff_role': staff_role.map(staff_role => staff_role.toJSON()),
+        },
+      });
+    }
+
+    return res.status(404).json({
+      code: 404,
+      message: 'There are no staff roles.',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- models/staff_skills.js
+++ models/staff_skills.js
@@ -29,3 +29,75 @@ StaffSkills.belongsTo(StaffDetails, { foreignKey: 'staff_id', as: 'staff' });
 StaffSkills.belongsTo(SkillDetails, { foreignKey: 'skill_id', as: 'skill' });
 
 module.exports = StaffSkills;
+
+
+
+// --Microservice Portion--
+const express = require('express');
+const cors = require('cors');
+const Sequelize = require('sequelize');
+
+const app = express();
+const port = 5002;
+
+app.use(cors());
+
+const sequelize = new Sequelize(process.env.dbURL || 'mysql://root@localhost:3306/spm', {
+  logging: false,
+});
+
+app.get('/staff_skills', async (req, res) => {
+  try {
+    const skill_list = await staff_skills.findAll();
+
+    if (skill_list.length) {
+      return res.status(200).json({
+        code: 200,
+        data: {
+          'skill_list': skill_list.map(staff_skills => staff_skills.toJSON()),
+        },
+      });
+    }
+
+    return res.status(404).json({
+      code: 404,
+      message: 'There are no available houses.',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+app.get('/staff_skills/:staff_id', async (req, res) => {
+  const staff_skills = req.params.staff_id;
+
+  try {
+    const staff_skills = await spm.findOne({ where: { staff_id } });
+
+    if (staff_skills) {
+      return res.status(200).json({
+        code: 200,
+        data: staff_skills.toJSON(),
+      });
+    }
+
+    return res.status(404).json({
+      code: 404,
+      message: 'Staff skills not found.',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
