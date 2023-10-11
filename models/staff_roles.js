73edbc0fcb21577a7845d2b95c23d642e6fba4c0
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-10-11 09:38:48 +0800

    commented out microservice parts for now, testing monolthic service first.

--- models/staff_roles.js
+++ models/staff_roles.js
@@ -32,66 +32,66 @@ StaffRoles.belongsTo(RoleDetails, { foreignKey: 'staff_role', as: 'role' });
 
 module.exports = StaffRoles;
 
-// --Microservice Portion--
-const express = require('express');
-const mysql2 = require('mysql2');
+// // --Microservice Portion--
+// const express = require('express');
+// const mysql2 = require('mysql2');
 
-const app = express();
-const port = 5003;
+// const app = express();
+// const port = 5003;
 
 
-app.get('/staff_roles', async (req, res) => {
-  try {
-    const staff_roles = await StaffRoles.findAll();
+// app.get('/staff_roles', async (req, res) => {
+//   try {
+//     const staff_roles = await StaffRoles.findAll();
 
-    if (staff_roles.length) {
-      return res.status(200).json({
-        code: 200,
-        data: {
-          'staff_roles': staff_roles.map(staff_roles => staff_roles.toJSON()),
-        },
-      });
-    }
+//     if (staff_roles.length) {
+//       return res.status(200).json({
+//         code: 200,
+//         data: {
+//           'staff_roles': staff_roles.map(staff_roles => staff_roles.toJSON()),
+//         },
+//       });
+//     }
 
-    return res.status(404).json({
-      code: 404,
-      message: 'There are no available staff roles.',
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: 'Internal Server Error',
-    });
-  }
-});
+//     return res.status(404).json({
+//       code: 404,
+//       message: 'There are no available staff roles.',
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({
+//       code: 500,
+//       message: 'Internal Server Error',
+//     });
+//   }
+// });
 
-app.get('/staff_roles/:staff_id', async (req, res) => {
-  const staff_id = req.params.staff_id;
-  try {
-    const staff_roles = await StaffRoles.findAll({where: {staff_id}});
-    if (staff_roles) {
-      return res.status(200).json({
-        code: 200,
-        data: {
-          'staff_roles': staff_roles.map(staff_roles => staff_roles.toJSON()),
-        },
-      });
-    }
+// app.get('/staff_roles/:staff_id', async (req, res) => {
+//   const staff_id = req.params.staff_id;
+//   try {
+//     const staff_roles = await StaffRoles.findAll({where: {staff_id}});
+//     if (staff_roles) {
+//       return res.status(200).json({
+//         code: 200,
+//         data: {
+//           'staff_roles': staff_roles.map(staff_roles => staff_roles.toJSON()),
+//         },
+//       });
+//     }
 
-    return res.status(404).json({
-      code: 404,
-      message: 'There are no available staff roles / Staff does not exist.',
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: 'Internal Server Error',
-    });
-  }
-});
+//     return res.status(404).json({
+//       code: 404,
+//       message: 'There are no available staff roles / Staff does not exist.',
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({
+//       code: 500,
+//       message: 'Internal Server Error',
+//     });
+//   }
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
