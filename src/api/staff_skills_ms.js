a46f64db8844db62393fbdf5ea84a8d50b6a953b
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-10-11 10:07:49 +0800

    staff_skills_ms is simple microservice on api folder.

---
+++ src/api/staff_skills_ms.js
@@ -0,0 +1,92 @@
+// --Microservice Portion--
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../models/ConnectionManager'); // Set up your Sequelize connection
+const StaffDetails = require('../../models/staff_details'); // Import the StaffDetails model
+const SkillDetails = require('../../models/skill_details'); // Import the SkillDetails model
+const express = require('express');
+const mysql2 = require('mysql2');
+
+const app = express();
+const port = 5002;
+
+
+const StaffSkills = sequelize.define('StaffSkills', {
+    staff_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    skill_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    ss_status: {
+      type: DataTypes.ENUM('active', 'unverified', 'in-progress'),
+      allowNull: false,
+    },
+  }, {
+    tableName: 'STAFF_SKILLS',
+    timestamps: false,
+  });
+  
+  StaffSkills.belongsTo(StaffDetails, { foreignKey: 'staff_id', as: 'staff' });
+  StaffSkills.belongsTo(SkillDetails, { foreignKey: 'skill_id', as: 'skill' });
+  
+  module.exports = StaffSkills;
+  
+app.get('/staff_skills', async (req, res) => {
+  try {
+    const skill_list = await StaffSkills.findAll();
+
+    if (skill_list.length) {
+      return res.status(200).json({
+        code: 200,
+        data: {
+          'skill_list': skill_list.map(staff_skills => staff_skills.toJSON()),
+        },
+      });
+    }
+
+    return res.status(404).json({
+      code: 404,
+      message: 'There are no available skills.',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+app.get('/staff_skills/:staff_id', async (req, res) => {
+  const staff_id = req.params.staff_id;
+
+  try {
+    const staff_skills = await StaffSkills.findOne({ where: { staff_id } });
+
+    if (staff_skills) {
+      return res.status(200).json({
+        code: 200,
+        data: staff_skills.toJSON(),
+      });
+    }
+
+    return res.status(404).json({
+      code: 404,
+      message: 'Staff skills / Staff details not found.',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
