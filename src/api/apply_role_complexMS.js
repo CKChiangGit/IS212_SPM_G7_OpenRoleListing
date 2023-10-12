00376700bd7260ccb574e97fffc945d9557c2859
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-10-12 19:09:47 +0800

    Moved matching algo to apply_role_complexMS.js

--- src/api/apply_role_complexMS.js
+++ src/api/apply_role_complexMS.js
@@ -5,10 +5,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser'); 
 
 // Import the Sequelize instance from ConnectionManager
-const sequelize = require('./models/ConnectionManager'); 
+const sequelize = require('../../models/ConnectionManager'); 
 
 // Import the Sequelize model for RoleApplications
-const RoleApplications = require('./models/role_applications'); 
+const RoleApplications = require('../../models/role_applications'); 
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -17,9 +17,9 @@ app.use(bodyParser.json());
 
 //const skills_results = require('./get_staff_skill.js');
 //const role_skills=require('./role_skills.js'); 
-const role_details=require('./role_details.js');
-const staff_details=require('./staff_details.js');
-const role_application_ids=require('./role_applications.js'); 
+const role_details=require('../../role_details.js');
+const staff_details=require('../../staff_details.js');
+const role_application_ids=require('../../role_applications.js'); 
 
 //viewing role applications for HR
 app.get('/role_applications', async (req, res) => {
@@ -75,3 +75,10 @@ app.listen(PORT, async () => {
 
 
 
+// Matching algo code test from chatgpt, havent tested with MS communication yet.
+// function jaccardSimilarity(staff_skills, role_skills) {
+//   const intersection = staff_skills.filter(skill => role_skills.includes(skill));
+//   const union = [...new Set([...staff_skills, ...role_skills])];
+//   const similarity = intersection.length / union.length;
+//   return similarity;
+// }
