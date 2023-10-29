213dd3c19f01f0a3aac4276a9cd4d79c928f1230
Dawntwister <ngzhanhui9@gmail.com>
2023-10-29 18:01:06 +0800

    Updated staffskills.js to include retrieve data from database

--- src/api/staffskills.js
+++ src/api/staffskills.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('../../models/ConnectionManager');
 const StaffDetails = require('../../models/staff_skills');
+const SkillNames = require('../models/skill_details');
 const cors = require('cors');
 
 const app = express();
