88ac61c72f5d866d077e5e5ce4dba8e546fc9175
CKChiangGit <88221395+CKChiangGit@users.noreply.github.com>
2023-10-29 15:10:24 +0800

    added setUp() and tearDown()

--- src/tests/test_complexMS.py
+++ src/tests/test_complexMS.py
@@ -1,7 +1,34 @@
 import unittest
+import psycopg2
 import requests
 
 class TestApplyRole(unittest.TestCase):
+    def setUp(self):
+        # # need install psycopg2-binary
+        
+        # # Connect to the database
+        # conn = psycopg2.connect(
+        #     host="<hostname>",
+        #     port="<port>",
+        #     user="<username>",
+        #     password="<password>",
+        #     database="<database>"
+        # )
+
+        # # Create a cursor object
+        # cur = conn.cursor()
+
+        # # Execute the SQL command to create the database
+        # cur.execute("CREATE DATABASE <database>")
+
+        # # Close the cursor and connection
+        # cur.close()
+        # conn.close()
+        pass
+    
+    def tearDown(self):
+        pass
+    
     def test_apply_role(self):
         staffId = 8857
         response = requests.get(f'http://localhost:5001/apply_role/8857')
