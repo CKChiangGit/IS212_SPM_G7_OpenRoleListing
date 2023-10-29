7f7111d28dfe074eac57566ac6aa7041a15ee183
CKChiangGit <88221395+CKChiangGit@users.noreply.github.com>
2023-10-29 11:26:53 +0800

    create unit tests for complex ms

---
+++ src/tests/test_complexMS.py
@@ -0,0 +1,14 @@
+import unittest
+import requests
+
+class TestApplyRole(unittest.TestCase):
+    def test_apply_role(self):
+        staffId = 8857
+        response = requests.get(f'http://localhost:5001/apply_role/{staffId}')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('result', response.json())
+        self.assertIsInstance(response.json()['result'], list)
+        self.assertGreater(len(response.json()['result']), 0)
+        
+if __name__ == '__main__':
+    unittest.main()
