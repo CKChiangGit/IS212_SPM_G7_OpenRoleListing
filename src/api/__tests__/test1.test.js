9d058ad222e9e6e239152101bf7814d443e1b106
pevangeline.2021@smu.edu.sg <pevangeline.2021@scis.smu.edu.sg>
2023-10-29 15:11:42 +0800

    unit tests for backend

---
+++ src/api/__tests__/test1.test.js
@@ -0,0 +1,27 @@
+/*const app = require('../../app');
+const request = require('supertest');
+
+describe('searching for roles', () => {
+    it('returns status code 200 if oepn roles list appears', async()=>{
+        const response = await request(app).get('/openroles');
+
+        expect(response.statusCode).toBe(200);
+    });
+});*/
+
+/*const app=require('../rolelistings');
+const request = require('supertest'); 
+describe('searching for roles', () => {
+    it('returns status code 200 if oepn roles list appears', async()=>{
+        const response = await request(app).get('/roledetails');
+
+        expect(response.statusCode).toBe(200);
+    });
+}); */
+
+const request = require('supertest');
+const baseurl='http://localhost:3001';
+test('testing for retreiving rolelistings', async () => {
+    const response = await request(baseurl).get('/rolelistings');
+    expect(response.statusCode).toBe(200);
+});
