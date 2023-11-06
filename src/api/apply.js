cf927720cf38b2f324546d9564a7a05b913a26ee
Dawntwister <ngzhanhui9@gmail.com>
2023-11-06 16:10:32 +0800

    Added HR UI appy listings

--- src/api/apply.js
+++ src/api/apply.js
@@ -69,6 +69,68 @@ app.get('/staffroleapp/:staff_id', async (req, res) => {
     }
 });
 
+app.get('/hrroleapp/:staff_id', async (req, res) => {
+    // Check if staff is hr/manager
+    const staff_id = req.params.staff_id;
+    const staff = await StaffDetails.findOne({
+        where: {
+            staff_id
+        }
+    });
+    const staffRole = staff.sys_role;
+    if (staffRole !== "hr" || staffRole !== "manager") {
+        res.status(403).json ({message: "You are not authorized to view this page."});
+    } else {
+        try {
+            const roleApplications = await RoleApplications.findAll({
+                where: {
+                    role_app_status: "applied"
+                }
+            });
+            
+            const appIds = roleApplications.map(app => app.role_listing_id);
+            const getroleListings = await RoleListings.findAll({
+                where: {
+                    role_listing_id: appIds
+                },
+                attributes: ['role_id', 'role_listing_desc', 'role_listing_source', 'role_listing_open', 'role_listing_close']
+            });      
+    
+            const map_list = {
+                application_id: roleApplications.map(app => app.role_app_id),
+                staff_applied: roleApplications.map(app => app.staff_id),
+                role_id: getroleListings.map(roleListing => roleListing.role_id),
+                role_listing_desc: getroleListings.map(roleListing => roleListing.role_listing_desc),
+                role_listing_source: getroleListings.map(roleListing => roleListing.role_listing_source),
+                role_listing_open: getroleListings.map(roleListing => roleListing.role_listing_open),
+                role_listing_close: getroleListings.map(roleListing => roleListing.role_listing_close),
+                application_status: roleApplications.map(app => app.role_app_status)
+            }
+    
+            const roleApplicationList = [];
+            // Count the number of values in map_list[0][1] and create the same number of objects in roleApplicationList
+            for (let i = 0; i < map_list.application_id.length; i++) {
+                roleApplicationList[i] = {};
+                // For each object in roleApplicationList, assign the key as the key in map_list and the value as the value in map_list[key][i]
+                for (const key in map_list) {
+                    roleApplicationList[i][key] = map_list[key][i];
+                }
+            }
+
+            if (!roleApplications.length) {
+                res.status(404).send('<p>There are no role applications.</p>');
+            }
+
+            res.status(200).json({
+                roleApplicationList
+            });
+        }  catch (error) {
+            console.error('Error getting all role applications:', error);
+            res.status(500).send(`<p>There is an internal error, please contact the IT Department Team.</p>`);
+        }
+    }
+});
+
 // Post a new role application
 app.post('/createroleapplications/:staff_id', async (req, res) => {
     try {
