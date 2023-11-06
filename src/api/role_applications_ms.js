3dee953614a0efe642954e0063350d93368826ba
Dawntwister <ngzhanhui9@gmail.com>
2023-11-06 15:57:36 +0800

    Updated Role Application Microservice

--- src/api/role_applications_ms.js
+++ src/api/role_applications_ms.js
@@ -1,102 +1,114 @@
-// -- Code from Persis -- 
-const express = require('express'); 
-const cors = require('cors');
+// This microservice is responsible for the sending in of job listings.
+
+// Import the necessary modules and database models to use
+const express = require('express');
 const bodyParser = require('body-parser');
-
-// Import the Sequelize instance from ConnectionManager
-const sequelize = require('../../models/ConnectionManager');
-
-// Import the Sequelize model for RoleDetails
-const RoleApplications = require('../../models/role_applications');
-
+const sequelize = require('../models/ConnectionManager');
+const RoleApplications = require('../models/role_applications');
+const RoleListings = require('../models/role_listings');
+const cors = require('cors');
 const app = express();
+
+// Define the port number
 const PORT = process.env.PORT || 5002;
-app.use(cors())
-app.use(bodyParser.json()); 
 
-app.get('/role_applications_ids', async (req, res) => {
-    var role_app_ids=[]
+// Use the necessary modules
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/staffroleapp/:staff_id', async (req, res) => {
     try {
-      const role_applications = await RoleApplications.findAll();
-      var jsondata=JSON.stringify(role_applications);  
-        var jsondata1=JSON.parse(jsondata)
-        for(var data1 in jsondata1){
-            for(var key in jsondata1[data1]){
-                if(key==='role_app_id'){
-                    role_app_ids.push(Number(jsondata1[data1][key]))
-                }
-              }
+        const roleApplications = await RoleApplications.findAll({
+            where: {
+                staff_id: req.params.staff_id
             }
-    } catch (error) {
-      res.status(500).send(`Internal server error in '/role_applications_ids' endpoint`);
-    }
-  });
-// Post a new role application
-app.post('/roleapplications', async (req, res) => {
-  try {
-      const { role_app_id, role_listing_id, staff_id, role_app_status } = req.body;
-      const roleApplication = await RoleApplications.create({
-      role_app_id,
-      role_listing_id,
-      staff_id,
-      role_app_status
-      });
-    } catch (error) { 
-      console.error('Error creating role application:', error);
-      res.status(500).json({ error: `Internal server error in '/roleapplications' endpoint` });
-    }
-  });
-
-  app.post('/role_applications_staff', async (req, res) => {
-    try {
-      const { staff_id, role_listing_id } = req.body;
-      
-      // Create a new role application
-      const role_application = await RoleApplications.create({
-        staff_id,
-        role_listing_id,
-        role_app_status: 'applied',
-        role_app_ts_create: new Date().toISOString(),
-      });
-  
-      res.status(201).json(role_application); // Respond with the newly created role application
-    } catch (error) {
-      console.error('Error creating role application:', error);
-      res.status(500).json({ error: `Internal server error in '/role_applications' endpoint` });
-    }
-  });
-
-
-  app.put('/role_applications/:role_app_id', async (req, res) => {
-    try {
-      const { role_app_id } = req.params; // Extract the role_app_id from the URL parameters
-      const { newStatus } = req.body; // Define a newStatus field in the request body
-  
-      // Check if the newStatus is one of the allowed values ('withdrawn', 'accepted', 'rejected')
-      if (newStatus === 'withdrawn' || newStatus === 'accepted' || newStatus === 'rejected') {
-        const roleApplication = await RoleApplications.findOne({
-          where: { role_app_id },
         });
-  
-        if (roleApplication) {
-          // Update the role_app_status with the newStatus
-          roleApplication.role_app_status = newStatus;
-  
-          // Save the updated role application to the database
-          await roleApplication.save();
-  
-          res.status(200).json(roleApplication); // Respond with the updated role application
-        } else {
-          res.status(404).json({ error: 'Role application not found' });
+        
+        const appIds = roleApplications.map(app => app.role_listing_id);
+        const getroleListings = await RoleListings.findAll({
+            where: {
+                role_listing_id: appIds
+            },
+            attributes: ['role_id', 'role_listing_desc', 'role_listing_source', 'role_listing_open', 'role_listing_close']
+        });      
+
+        const map_list = {
+            application_id: roleApplications.map(app => app.role_app_id),
+            staff_applied: roleApplications.map(app => app.staff_id),
+            role_id: getroleListings.map(roleListing => roleListing.role_id),
+            role_listing_desc: getroleListings.map(roleListing => roleListing.role_listing_desc),
+            role_listing_source: getroleListings.map(roleListing => roleListing.role_listing_source),
+            role_listing_open: getroleListings.map(roleListing => roleListing.role_listing_open),
+            role_listing_close: getroleListings.map(roleListing => roleListing.role_listing_close),
+            application_status: roleApplications.map(app => app.role_app_status)
         }
-      } else {
-        res.status(400).json({ error: 'Invalid newStatus value' });
-      }
+
+        const roleApplicationList = [];
+        // Count the number of values in map_list[0][1] and create the same number of objects in roleApplicationList
+        for (let i = 0; i < map_list.application_id.length; i++) {
+            roleApplicationList[i] = {};
+            // For each object in roleApplicationList, assign the key as the key in map_list and the value as the value in map_list[key][i]
+            for (const key in map_list) {
+                roleApplicationList[i][key] = map_list[key][i];
+            }
+        }
+            
+
+        if (!roleApplications.length) {
+            res.status(404).send('<p>There are no role applications.</p>');
+        }
+
+        res.status(200).json({
+            roleApplicationList
+        });
+
     } catch (error) {
-      console.error('Error updating role application status:', error);
-      res.status(500).json({ error: 'Internal server error in updating role application status' });
+        console.error('Error getting all role applications:', error);
+        res.status(500).send(`<p>There is an internal error, please contact the IT Department Team.</p>`);
     }
-  });
+});
+
+// Post a new role application
+app.post('/createroleapplications/:staff_id', async (req, res) => {
+    try {
+        const { role_app_id, role_listing_id, staff_id, role_app_status } = req.body;
+        const roleApplication = await RoleApplications.create({
+        role_app_id,
+        role_listing_id,
+        staff_id,
+        role_app_status
+        });
+        if (roleApplication.role_app_status == "applied") {
+            res.status(500).json({message: "You have already applied for this role."});
+        }     
+
+        res.status(201).json(roleApplication);
+    } catch (error) {
+        console.error('Error creating a new role application:', error);
+        res.status(500).send(`<p>There is an internal error, please contact the IT Department Team.</p>`);
+    }
+});
+
+// Update a role application
+app.put('/updateroleapplications/:role_app_id', async (req, res) => {
+    try {
+        const { role_app_status } = req.body;
+        const roleApplication = await RoleApplications.update({
+            role_app_status
+        }, {
+            where: {
+                role_app_id: req.params.role_app_id
+            }
+        });
+
+        res.status(200).json(roleApplication);
+    } catch (error) {
+        console.error('Error updating a role application:', error);
+        res.status(500).send(`<p>There is an internal error, please contact the IT Department Team.</p>`);
+    }
+});
+
 
 app.listen(PORT, async () => {
     try {
@@ -108,4 +120,5 @@ app.listen(PORT, async () => {
     } catch (error) {
       console.error('Error syncing the model:', error);
     }
-  }); 
+  });  
+
